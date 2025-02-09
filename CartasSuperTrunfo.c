554aac1c6360aa143934f09e1cb69f36526335ab
Cassia1975 <cassiapereira1501@gmail.com>
2025-02-09 20:01:13 +0000

    NIVEL INTERMEDIARIO - CALCULO PIB PER CAPTA E DENSIDADE POP

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,48 +7,64 @@
 //Teste Cassia
 //Não soube como arrumar este erro na linha 11, corrijo, salvo e não salva, para corrigir: Delete e Tab.
     
-    int main(){
+   // Função que calcula a densidade populacional e PIB per capita
 
-    printf("Super Trunfo - Paises - Cadastro de Cartas \n\n");
-    printf("Sr(a) usuario, insira os dados de cada carta, de oito estados identificados pelas letras de A a H. Cada estado tera quatro cidades, numeradas de 1 a 4");
-    printf("\n\n'Cartas'\n\n");
-
-    char estad[20] = "A,B,C,D,E,F,G,H"; //Estados identificados pelas letras A a H.
-    char codig_da_carta[100] = "A01;A02;B01;B02"; //Codigos das cartas (A01,A02,B01,B2).
-    char nome[50] = "01,02,03,04"; //Cidades numeradas de 1 a 4.
+void calcularDensidadePopulacionalEPibPercapita() {
     int populacao;
     float area;
     float PIB;
+    float densidadePopulacional;
+    float PIBPercapita;
     int pontos;
 
-    //O comando 'for' permite que variável seja executada 34 vezez (8 estados X 4 cidades)
-    for (int carta = 1; carta <= 34; carta++) {
-        printf("\nCarta %d\n", carta);
-        
-    //Entrada e saída de dados
-    printf("Digite o Estado:", estad);
-    scanf("%s", &estad);
-
-    printf("Digite o Codigo da Carta:", codig_da_carta);
-    scanf("%s", &codig_da_carta);
-
-    printf("Digite Nome da Cidade:", nome); 
-    scanf(" %[^\n]", &nome); // Lê o nome da cidade incluindo espaços e acentuação
-
-
-    printf("Digite a Populacao:", populacao);
+    printf("Digite a Populacao: ");
     scanf("%d", &populacao);
 
-    printf("Digite a Area em km2:", area);
+    printf("Digite a Area em km²: ");
     scanf("%f", &area);
 
-    printf("Digite o PIB:", PIB);
+    printf("Digite o PIB: ");
     scanf("%f", &PIB);
 
-    printf("Digite a quantidade de Pontos Turisticos:", pontos);
+    densidadePopulacional = populacao / area;
+    PIBPercapita = PIB / populacao;
+
+    // Exibe os resultados
+    printf("Densidade populacional: %.2f habitantes/km²\n", densidadePopulacional);
+    printf("PIB per Capita: %.2f\n", PIBPercapita);
+
+    printf("Digite a quantidade de pontos tustisticos:", pontos);
     scanf("%d", &pontos);
+}
+
+int main() {
+    printf("Super Trunfo - Paises - Cadastro de Cartas \n\n");
+    printf("Sr(a) usuario, insira os dados de cada carta, de oito estados identificados pelas letras de A a H.Cada estado tera quatro cidades, numeradas de 1 a 4\n\n");
+
+    char estad[20]; // Estados identificados pelas letras A a H.
+    char codig_da_carta[100]; // Codigos das cartas (A01,A02,B01,B02).
+    char nome[50]; // Cidades numeradas de 1 a 4.
+
+    //O comando for permite que variável seja executada 34 vezez (8 estados X 4 cidades)
+    for (int carta = 1; carta <= 34; carta++) {
+        printf("\nCarta %d\n", carta);
+
+        printf("Digite o Estado: ");
+        scanf("%s", &estad);
+
+        printf("Digite o Codigo da Carta: ");
+        scanf("%s", &codig_da_carta);
+
+        printf("Digite o Nome da Cidade: ");
+        scanf(" %[^\n]", &nome);  // Lê o nome da cidade incluindo espaços e acentuação
+
+        calcularDensidadePopulacionalEPibPercapita(); // Chama a função para calcular e mostrar a densidade populacional e o PIB per capita
 
     }
+
+    return 0;
+}
+
 
        
     // Cadastro das Cartas:
@@ -58,5 +74,4 @@
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-    return 0;
-}
+    
